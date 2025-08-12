4cd2f48d3a58998ca45f6f93f318c39d26684ac1
daniel <daniel.stoyanovv@gmail.com>
2025-08-12 20:49:29 +0300

    Add Index.php - implement index controller logic Update Restaurant.php - clear frontend route

---
+++ app/code/Stoyanov/Restaurants/Controller/Restaurant/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stoyanov\Restaurants\Controller\Restaurant;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    public function __construct(
+        Context $context,
+        private PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}

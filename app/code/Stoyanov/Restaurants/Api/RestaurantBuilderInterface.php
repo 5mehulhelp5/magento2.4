6a808b91ed37a7b68b3bf8bf758bf14ff97fc3e6
daniel <daniel.stoyanovv@gmail.com>
2025-08-19 10:16:46 +0300

    Update Create controller - use RestaurantRepositoryInterface in logic implement calculateEntityIdValue method and use it in logic Update Data.php - use RestaurantBuilderInterface in logic implement buildRestaurant method use buildRestaurant in createRestaurant logic - use buildRestaurant in updateRestaurant logic Update di.xml - add new preference for RestaurantBuilderInterface Update RestaurantBuilder.php - add RestaurantBuilderInterface Add RestaurantBuilderInterface.php

---
+++ app/code/Stoyanov/Restaurants/Api/RestaurantBuilderInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Stoyanov\Restaurants\Api;
+
+use Stoyanov\Restaurants\Model\Restaurant;
+
+interface RestaurantBuilderInterface
+{
+    public function setId(int $id): \Stoyanov\Restaurants\Model\RestaurantBuilder;
+
+    public function setName(string $name): \Stoyanov\Restaurants\Model\RestaurantBuilder;
+
+    public function setCapacity(int $capacity): \Stoyanov\Restaurants\Model\RestaurantBuilder;
+
+    public function setLocation(string $location): \Stoyanov\Restaurants\Model\RestaurantBuilder;
+
+    public function setCreatedAt(string $createdAt): \Stoyanov\Restaurants\Model\RestaurantBuilder;
+
+    public function build(): Restaurant;
+}
--- app/code/Stoyanov/Restaurants/Helper/Data.php
+++ app/code/Stoyanov/Restaurants/Helper/Data.php
@@ -6,8 +6,8 @@ namespace Stoyanov\Restaurants\Helper;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Stoyanov\Restaurants\Api\RestaurantBuilderInterface;
 use Stoyanov\Restaurants\Api\RestaurantRepositoryInterface;
-use Stoyanov\Restaurants\Model\RestaurantBuilder;
 use Stoyanov\Restaurants\Model\Restaurant;
 
 class Data extends AbstractHelper
@@ -21,7 +21,7 @@ class Data extends AbstractHelper
         Context                       $context,
         RestaurantRepositoryInterface $restaurantRepository,
         TimezoneInterface             $timezone,
-        RestaurantBuilder             $builder,
+        RestaurantBuilderInterface    $builder,
     ) {
         $this->restaurantRepository = $restaurantRepository;
         $this->timezone             = $timezone;
@@ -31,29 +31,11 @@ class Data extends AbstractHelper
     }
 
     /**
-     * @param $countRestaurants
-     * @return int
-     */
-    private function calculateEntityIdValue($countRestaurants): int
-    {
-        $entityId = $countRestaurants + 1;
-        if ($countRestaurants === 0) $entityId = 1;
-        return $entityId;
-    }
-
-    /**
      * @return mixed
      */
-    public function createRestaurant(): mixed
+    public function createRestaurant($data): mixed
     {
-        $countRestaurants = $this->restaurantRepository->getList()->getSize();
-        $restaurant = $this->builder
-            ->setId($this->calculateEntityIdValue($countRestaurants))
-            ->setName($this->_request->getPost("name"))
-            ->setCapacity((int) $this->_request->getPost("capacity"))
-            ->setLocation($this->_request->getPost("location"))
-            ->setCreatedAt($this->timezone->date()->format('Y-m-d H:i:s'))
-            ->build();
+        $restaurant = $this->buildRestaurant($data);
         $response = $this->restaurantRepository->save($restaurant);
         return $response;
     }
@@ -69,14 +51,23 @@ class Data extends AbstractHelper
 
     public function updateRestaurant($data): mixed
     {
-        $restaurant = $this->builder
+        $restaurant = $this->buildRestaurant($data);
+        $response = $this->restaurantRepository->save($restaurant);
+        return $response;
+    }
+
+    /**
+     * @param $data
+     * @return Restaurant
+     */
+    private function buildRestaurant($data): Restaurant
+    {
+        return $this->builder
             ->setId((int) $data["id"])
             ->setName($data["name"])
             ->setCapacity((int) $data["capacity"])
             ->setLocation($data["location"])
             ->setCreatedAt($this->timezone->date()->format('Y-m-d H:i:s'))
             ->build();
-        $response = $this->restaurantRepository->save($restaurant);
-        return $response;
     }
 }

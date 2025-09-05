878317f53a355b378ef7c9e0590aaf3ad646665c
daniel <daniel.stoyanovv@gmail.com>
2025-09-05 12:00:50 +0300

    Update RestaurantInterface.php - add dock blocks for each method Update RestaurantRepositoryInterface.php - add dock blocks for each method Create webapi.xml - configure restaurants rest api

--- app/code/Stoyanov/Restaurants/Api/Data/RestaurantInterface.php
+++ app/code/Stoyanov/Restaurants/Api/Data/RestaurantInterface.php
@@ -10,19 +10,47 @@ interface RestaurantInterface
     const LOCATION = 'location';
     const CREATED_AT = 'created_at';
 
+    /**
+     * @return mixed
+     */
     public function getId();
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function setId($id);
 
+    /**
+     * @return mixed
+     */
     public function getName();
 
+    /**
+     * @param $name
+     * @return mixed
+     */
     public function setName($name);
 
+    /**
+     * @return mixed
+     */
     public function getCapacity();
 
+    /**
+     * @param $capacity
+     * @return mixed
+     */
     public function setCapacity($capacity);
 
+    /**
+     * @return mixed
+     */
     public function getLocation();
 
+    /**
+     * @param $location
+     * @return mixed
+     */
     public function setLocation($location);
 }

76e7a7d862274aa19b5eec888580cc2f1d859a64
daniel <daniel.stoyanovv@gmail.com>
2025-09-15 10:13:41 +0300

    Update index.phtml - add getPrevPage method in template - add getNextPage method in template Update RestaurantsData.php - implement getCurrentPage method - implement getNextPage method - implement getPrevPage method - implement getPageSize method - implement getCollectionCount method Update RestaurantsDataAdmin.php - add method return type

--- app/code/Stoyanov/Restaurants/ViewModel/RestaurantsData.php
+++ app/code/Stoyanov/Restaurants/ViewModel/RestaurantsData.php
@@ -8,25 +8,50 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Stoyanov\Restaurants\Model\ResourceModel\Restaurant;
 use Magento\Framework\App\RequestInterface;
 use Stoyanov\Restaurants\Api\RestaurantManagerInterface;
+use Stoyanov\Restaurants\Helper\Data;
 
 class RestaurantsData implements ArgumentInterface
 {
     public function __construct(
         private RestaurantManagerInterface $manager,
-        private RequestInterface $request
+        private RequestInterface $request,
+        private Data $data,
     ) {
     }
 
     /**
      * @return Collection
      */
-    public function getRestaurants(): Restaurant\Collection
+    function getRestaurants(): Restaurant\Collection
     {
         return $this->manager->getRestaurants($this->getCurrentPage(), true);
     }
 
-    public function getCurrentPage()
+    private function getCurrentPage() : int
     {
         return (int) $this->request->getParam('p', 1);
+    }
+    function getNextPage(): int
+    {
+        $nextPage = $this->getCurrentPage() + 1;
+        if ($nextPage >  $this->getCollectionCount() / $this->getPageSize()) $nextPage = $this->getCurrentPage();
+        return $nextPage;
+    }
+
+    function getPrevPage(): int
+    {
+        $prevPage = $this->getCurrentPage() - 1;
+        if ($prevPage < 1) $prevPage = 1;
+        return $prevPage;
+    }
+
+    private function getPageSize(): int
+    {
+        return (int) $this->data->getConfigValue("page_size");
+    }
+
+    private function getCollectionCount(): int
+    {
+        return (int) $this->getRestaurants()->getSize();
     }
 }

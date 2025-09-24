f329c0fb0ae0f2aa279c523e76a166df7ebab86d
daniel <daniel.stoyanovv@gmail.com>
2025-09-24 16:23:48 +0300

    Update Edit.php - use $_coreRegistry in construct Update Index.php - use $_coreRegistry in construct Update NewAction.php - use $_coreRegistry in construct Update restaurants_restaurant_form.xml - add entity_id in configuration Update restaurants_restaurant_formnew.xml - add entity_id in configuration Update restaurants_restaurant_listing.xml - use entity_id in configuration Update Save.php - use $_coreRegistry in construct

--- app/code/Stoyanov/Restaurant/Controller/Adminhtml/Restaurant/Index.php
+++ app/code/Stoyanov/Restaurant/Controller/Adminhtml/Restaurant/Index.php
@@ -6,15 +6,18 @@ namespace Stoyanov\Restaurant\Controller\Adminhtml\Restaurant;
 use Magento\Backend\App\Action;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-
+use Magento\Framework\Registry;
 
 class Index extends Action implements HttpGetActionInterface
 {
     const ADMIN_RESOURCE = 'Stoyanov_Restaurant::restaurant_view';
 
-    public function __construct(Action\Context $context, protected PageFactory $resultPageFactory)
-    {
-        parent::__construct($context);
+    public function __construct(
+        Action\Context $context,
+        protected PageFactory $resultPageFactory,
+        protected Registry $coreRegistry,
+    ) {
+        parent::__construct($context, $coreRegistry);
     }
 
     public function execute()
--- app/code/Stoyanov/Restaurant/Controller/Adminhtml/Restaurant/Save.php
+++ app/code/Stoyanov/Restaurant/Controller/Adminhtml/Restaurant/Save.php
@@ -6,6 +6,7 @@ namespace Stoyanov\Restaurant\Controller\Adminhtml\Restaurant;
 use Magento\Backend\App\Action;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Registry;
 
 class Save extends Action implements HttpGetActionInterface
 {
@@ -13,9 +14,10 @@ class Save extends Action implements HttpGetActionInterface
 
     public function __construct(
         protected Action\Context $context,
-        protected PageFactory $resultPageFactory
+        protected PageFactory $resultPageFactory,
+        protected Registry $coreRegistry,
     ) {
-        parent::__construct($context);
+        parent::__construct($context, $coreRegistry);
     }
 
     public function execute()
